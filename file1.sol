05ce29a09f4e58458303d1e42926edbc3702506e
Konark Verma <kon172verma@gmail.com>
2021-11-04 02:56:00 +0530

    basic solidity contract

--- file1.sol
+++ file1.sol
@@ -1,14 +1,20 @@
 // SPDX-License-Identifier: GPL-3.0
+
+// This line tells our program to use which version of solidity for compilation.
 pragma solidity >=0.7.0 <0.9.0;
 
-contract Konark {
-    uint256 private number;
+// Created a contract named Temp1 that stores a Value and allows the user to see and assign a new value.
+contract Temp1 {
+    // Value: private variable is used to store a value.
+    uint256 private value = 7;
 
-    function setNumber(uint256 num) public {
-        number = num;
+    // Declared a function to get the value of the variable value.
+    function getValue() public view returns (uint256) {
+        return value;
     }
 
-    function getNumber() public view returns (uint256) {
-        return number;
+    // Declared a function to set a value of the variable value.
+    function setValue(uint256 val) public {
+        value = val;
     }
 }
