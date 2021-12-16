d9d7cd097ec42db2bfc62828d0c5ad67dda34e55
Konark Verma <kon172verma@gmail.com>
2021-12-16 12:05:44 +0530

    Updated Inbox.sol

--- Inbox/contracts/Inbox.sol
+++ Inbox/contracts/Inbox.sol
@@ -1,4 +1,17 @@
 pragma solidity ^0.4.17;
-// linter warnings (red underline) about pragma version can igonored!
 
-// contract code will go here
+contract Inbox{
+    string public message;
+
+    function Inbox (string initialMessage) public {
+        message = initialMessage;
+    }
+
+    function setMessage (string newMessage) public {
+        message = newMessage;
+    }
+
+    function getMessage() public view returns(string){
+        return message;
+    }
+}
