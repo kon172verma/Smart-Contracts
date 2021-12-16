6fdb31cfe0c7a0b1f514a3ff8fdd7a3cb6bccb5b
Konark Verma <kon172verma@gmail.com>
2021-12-16 12:05:59 +0530

    Updated compile.js

--- Inbox/compile.js
+++ Inbox/compile.js
@@ -1,1 +1,13 @@
-// compile code will go here
+// Importing the required libraries.
+const path = require('path');
+const fs = require('fs');
+const solc = require('solc');
+
+// Fetching the raw source code for our contract.
+const inboxPath = path.resolve(__dirname, 'contracts', 'Inbox.sol');
+const source = fs.readFileSync(inboxPath, 'utf8');
+
+// console.log(solc.compile(source, 1));
+
+// Exporting the compiled information for our contract.
+module.exports = solc.compile(source, 1).contracts[':Inbox'];
