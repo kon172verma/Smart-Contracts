15bbf00af2a13d323c30f297d94f77e9df4109f5
Konark Verma <kon172verma@gmail.com>
2021-12-28 14:10:17 +0530

    Updated deployment code

---
+++ Lottery/deploy.js
@@ -0,0 +1,20 @@
+const HDWalletProvider = require('@truffle/hdwallet-provider');
+const Web3 = require('web3');
+const provider = new HDWalletProvider(
+    'lawn trap weapon volume practice swift version length lab reflect nose flag',
+    'https://rinkeby.infura.io/v3/3ed112ee6c4d42a09e485ddb5eec5fa1'
+);
+const web3 = new Web3(provider);
+
+const { abi, evm } = require('./compile');
+
+const deploy = async () => {
+    const accounts = await web3.eth.getAccounts();
+    console.log('Attempting to deploy using account: ', accounts[0]);
+    const contract = await new web3.eth.Contract(abi)
+        .deploy({ data: evm.bytecode.object })
+        .send({ from: accounts[0], gas: '1000000' });
+    console.log('Contract has been deployed to: ', contract.options.address);
+    provider.engine.stop();
+}
+deploy();
