19b3b0c88b46a0511cb28c7a8f3dbdcd6478a5d1
Konark Verma <kon172verma@gmail.com>
2022-01-19 00:31:42 +0530

    updated index.js

--- Kickstarter/pages/index.js
+++ Kickstarter/pages/index.js
@@ -1,10 +1,21 @@
 import React from 'react';
 import web3 from '../web3';
+import instance from '../instance';
 
 class Index extends React.Component{
+
+    static async getInitialProps(){
+        const campaigns = await instance.methods.getCampaigns().call();
+        return { campaigns };
+    }
+
     render() {
         return (
-            <>Hello, this is the test page for now. Web3 version: {web3.version} </>
+            <>
+                <p>Hello, this is the test page for now.</p>
+                <p>Web3 version: {web3.version}</p>
+                <p>List of all the running campaigns: {this.props.campaigns} </p>
+            </>
         );
     }
 }
