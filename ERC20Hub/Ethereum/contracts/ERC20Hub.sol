389c9052c766b72d99b840cdf4ae43e4e3bd0620
Konark Verma <kon172verma@gmail.com>
2022-02-05 16:49:23 +0530

    ERC20 contract

---
+++ ERC20Hub/Ethereum/contracts/ERC20Hub.sol
@@ -0,0 +1,15 @@
+//SPDX-License-Identifier: MIT
+pragma solidity >=0.8 <0.9;
+import './ERC20Interface.sol';
+import './ERC20.sol';
+
+contract ERC20Hub{
+
+    address[] public ERC20Tokens;
+
+    function createERC20Token(string memory _name, string memory _symbol, uint8 _decimals, uint256 _totalSupply) public returns(bool){
+        ERC20 token = new ERC20(_name, _symbol, _decimals, _totalSupply, msg.sender);
+        ERC20Tokens.push(address(token));
+        return true;
+    }
+}
